86a4bb4c3acb7bb992811bc2126c5191586df65d
cpi <carla.perez@abstracta.com.uy>
2021-12-15 20:58:53 -0300

    simple login test

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,4 +1,4 @@
-import $ from 'webdriverio/build/commands/browser/$';
+//import $ from 'webdriverio/build/commands/browser/$';
 import loginPage from '../pages/login.page';
 
 describe('OpenBank', () => {
@@ -11,19 +11,20 @@ describe('OpenBank', () => {
     });
     it('Debería escribir usuario y contraseña e ingresar a home', async () => {
         await browser.url('login');
-        let usuarioInput = await $('[name="username"]');
-        console.log(await usuarioInput.getValue());
+        //let usuarioInput = await $('[name="username"]');
+        //console.log(await usuarioInput.getProperty('placeholder'));
         //let usuario = 'jsmith@demo.io';
         //let contraseña = 'Demo123!';
         //await loginPage.ingresar(usuario, contraseña);
-        /* let usuarioInput = await $('[name="username"]');
+        let usuarioInput = await $('[name="username"]');
         await usuarioInput.setValue('jsmith@demo.io');  //escribe en usuario
-        console.log(await usuarioInput.getValue());
+        //console.log(await usuarioInput.getValue());
         let contraseñaInput = await $('[name="password"]');
         await contraseñaInput.setValue('Demo123!');
-        await contraseñaInput.keys('Enter'); */
+        await contraseñaInput.keys('Enter');
         //await barraDeBusqueda.keys('Enter');  //presiona enter
         //await expect(await loginPage.obtenerTextoUsuario()).to.equal(usuario);
+        await assert.equal(await $('h1').getText(),'Dashboard','Error: no se mostró el header esperado');
         await browser.pause(5000);
      });
  });
