e65366debe29db65647185751b7ab3ed294ac401
cpi <carla.perez@abstracta.com.uy>
2021-12-17 14:01:32 -0300

    Login page object and test

--- tests/login.test.js
+++ tests/login.test.js
@@ -3,28 +3,38 @@ import loginPage from '../pages/login.page';
 
 describe('OpenBank', () => {
 
-    it('Debería mostrar la pagina login', async () => {
+    /* it('Debería mostrar la pagina login', async () => {
 
       browser.url('login');
 	  
 	  await browser.pause(5000);
-    });
-    it('Debería escribir usuario y contraseña e ingresar a home', async () => {
-        await browser.url('login');
+    }); */
+    it('Should write in username and password and enter the home page', async () => {
+        await loginPage.abrir('login');
+        //await browser.url('login');
         //let usuarioInput = await $('[name="username"]');
         //console.log(await usuarioInput.getProperty('placeholder'));
-        //let usuario = 'jsmith@demo.io';
-        //let contraseña = 'Demo123!';
-        //await loginPage.ingresar(usuario, contraseña);
-        let usuarioInput = await $('[name="username"]');
-        await usuarioInput.setValue('jsmith@demo.io');  //escribe en usuario
+        let usuario = 'jsmith@demo.io';
+        let contraseña = 'Demo123!';
+        await loginPage.ingresar(usuario, contraseña);
+       // let usuarioInput = await $('[name="username"]');
+        //await usuarioInput.setValue('jsmith@demo.io');  //escribe en usuario
         //console.log(await usuarioInput.getValue());
-        let contraseñaInput = await $('[name="password"]');
-        await contraseñaInput.setValue('Demo123!');
-        await contraseñaInput.keys('Enter');
+        //let contraseñaInput = await $('[name="password"]');
+        //await contraseñaInput.setValue('Demo123!');
+        //await contraseñaInput.keys('Enter');
         //await barraDeBusqueda.keys('Enter');  //presiona enter
         //await expect(await loginPage.obtenerTextoUsuario()).to.equal(usuario);
-        await assert.equal(await $('h1').getText(),'Dashboard','Error: no se mostró el header esperado');
+        await assert.equal(await $('h1').getText(),'Dashboard','Error: it did not show the expected text');
         await browser.pause(5000);
      });
+     it('Should show an error when atempting to login with invalid username and password', async () => {
+        await loginPage.abrir('login');
+        let usuario = 'asdfasdf';
+        let contraseña = '123456';
+        await loginPage.ingresar(usuario, contraseña);
+        //await expect(await $('.badge badge-pill badge-danger').getText()).to.include.string('Error','Error: it did not show the expected text');
+        //await assert.equal(await $('span.badge badge-pill badge-danger').getText(),'Error','Error: it did not show the expected text');
+        await browser.pause(5000);
+     })
  });
