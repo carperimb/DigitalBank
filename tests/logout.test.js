edb3a3ef75af5775e8c7c615a4e962b1cda84a65
cpi <carla.perez@abstracta.com.uy>
2021-12-28 10:18:06 -0300

    Logout changes

--- tests/logout.test.js
+++ tests/logout.test.js
@@ -1,32 +1,41 @@
 
+import { expect } from 'chai';
 import loginPage from '../pages/login.page';
 import logoutPage from '../pages/logout.page';
 
 describe('OpenBank', () => {
+  beforeEach(() => {
+      loginPage.open('login');
+      let username = 'jsmith@demo.io';
+      let password = 'Demo123!';
+      loginPage.logIn(username, password);
+
+  })
 
     it('Should show logout success message when clicking on logout menu', async () => {
 
-      await loginPage.open('login');
-      let username = 'jsmith@demo.io';
-      let password = 'Demo123!';
-      await loginPage.logIn(username, password);
+      
 
       await logoutPage.clickAvatar();
       
       expect(await browser.getUrl()).to.contain('login?logout');
+      //expect(await browser).toHaveUrlContaining('login?logout');
+      //await expect(browser).toHaveUrl('login?logout');
 
     });
     it('Should show login page when clicking browser back button after logging out the session', async () => {
-      await loginPage.open('login');
+      /* await loginPage.open('login');
       let username = 'jsmith@demo.io';
       let password = 'Demo123!';
-      await loginPage.logIn(username, password);
+      await loginPage.logIn(username, password); */
 
       await logoutPage.clickAvatar();
       
       await browser.back();
       
       expect(await browser.getUrl()).to.contain('login');
+      //expect(await browser).toHaveUrlContaining('login');
+      //await expect(browser).toHaveUrl('login');
 
 
     })
