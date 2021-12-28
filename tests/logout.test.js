9165b90cd682654406153fcffb2d53c3fc0ce7b1
cpi <carla.perez@abstracta.com.uy>
2021-12-28 13:00:50 -0300

    logout code review

--- tests/logout.test.js
+++ tests/logout.test.js
@@ -1,5 +1,3 @@
-
-
 import loginPage from '../pages/login.page';
 import logoutPage from '../pages/logout.page';
 
@@ -13,27 +11,16 @@ describe('OpenBank', () => {
   })
 
     it('Should show logout success message when clicking on logout menu', async () => {
-
-      
-
+ 
       await logoutPage.clickAvatar();
-      
-      
       expect(await browser).toHaveUrlContaining('login?logout');
       
-
     });
     it('Should show login page when clicking browser back button after logging out the session', async () => {
       
-
       await logoutPage.clickAvatar();
-      
       await browser.back();
-      
-      
       expect(await browser).toHaveUrlContaining('login');
       
-
-
     })
  });
